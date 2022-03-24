8ced08064748ef72ec0957690e5761090365de09
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-03-24 11:15:22 +0100

    Add text styles

--- src/Theme/index.ts
+++ src/Theme/index.ts
@@ -60,6 +60,61 @@ export const Theme = extendTheme({
       fontWeight: "normal",
       textTransform: "uppercase",
     },
+    button: {
+      fontFamily: "Roboto",
+      fontSize: ["16px"],
+      fontWeight: "bold",
+      letterSpacing: "3%",
+      textTransform: "uppercase",
+    },
+    h2: {
+      fontFamily: "Roboto",
+      fontSize: ["42px"],
+      fontWeight: "bold",
+      letterSpacing: "-2%",
+      textTransform: "uppercase",
+    },
+    h3: {
+      fontFamily: "Roboto",
+      fontSize: ["26px"],
+      fontWeight: "bold",
+      letterSpacing: "-2%",
+      textTransform: "uppercase",
+    },
+    body: {
+      fontFamily: "Roboto",
+      fontSize: ["16px"],
+      fontWeight: "normal",
+      lineHeight: "140%",
+    },
+    "body-bold": {
+      fontFamily: "Roboto",
+      fontSize: ["16px"],
+      fontWeight: "bold",
+      lineHeight: "140%",
+    },
+    small: {
+      fontFamily: "Roboto",
+      fontSize: ["14px"],
+      fontWeight: "normal",
+    },
+    "small-bold": {
+      fontFamily: "Roboto",
+      fontSize: ["14px"],
+      fontWeight: "bold",
+    },
+    xsmall: {
+      fontFamily: "Roboto",
+      fontSize: ["10px"],
+      fontWeight: "normal",
+      letterSpacing: "3%",
+    },
+    "xsmall-bold": {
+      fontFamily: "Roboto",
+      fontSize: ["10px"],
+      fontWeight: "bold",
+      letterSpacing: "3%",
+    },
   },
 });
 
