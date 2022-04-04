ca9300c54bbae43f0730cb35bd61a7b670a009b8
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-04-04 16:20:54 +0200

    Remove unused code

--- Seed/StainedGlass.ts
+++ Seed/StainedGlass.ts
@@ -1,8 +1,6 @@
 import { Artwork } from "../src/Types/Artwork";
 import { adaToLovelace, timestampAt, TokenID } from "./Helpers";
 
-export const devArtworks = ["64974370", "85843737", "93700844"];
-
 /**
  *
  * @param artwork the artwork
@@ -187,87 +185,3 @@ const stainedGlassNames = [
   "98763993",
   "99270561",
 ];
-
-// export const stainedGlass: Artwork[] = [
-//   {
-//     name: "#64974370",
-//     collection: "Stained Glass",
-//     saleInfo: {
-//       price: "25000000",
-//       status: "Available",
-//     },
-//     src: "images/64974370.svg",
-//     createdAt: timestampAt("2021-8-13"),
-//     tags: ["Generative", "Stained Glass", "Triangles"],
-//     token: {
-//       policyID: "fc7f00513d26a5c4de57a5863f0849559493d5ec008951eeb65ed3f1",
-//       assetName: "FHWALGOMARBLE0489",
-//       onChainMetadata: {
-//         name: "FHW64974370",
-//         image: "ipfs://QmVBrepGKCGgaMUEptswHdZtXUUxUWXeb2nA1GVjP2ze9p",
-//         mediaType: "image/png",
-//         files: [
-//           {
-//             src: "ipfs://QmSTuPM4DRaR9GfjBe3QyDtGZQxyc3D5DYjiVcPieUVupj",
-//             name: "FHW64974370",
-//             mediaType: "image/svg+xml",
-//           },
-//         ],
-//       },
-//     },
-//   },
-//   {
-//     name: "#85843737",
-//     collection: "Stained Glass",
-//     saleInfo: {
-//       price: "25000000",
-//       status: "Sold",
-//     },
-//     src: "images/85843737.svg",
-//     tags: ["Generative", "Stained Glass", "Triangles"],
-//     createdAt: timestampAt("2021-8-13"),
-//     token: {
-//       policyID: "fc7f00513d26a5c4de57a5863f0849559493d5ec008951eeb65ed3f1",
-//       assetName: "FHWALGOMARBLE0489",
-//       onChainMetadata: {
-//         name: "FHW85843737",
-//         image: "ipfs://QmYS2AhwR3nREa5MnoF2mnoTCGtZbC74BHiU8RTatbutS8",
-//         mediaType: "image/png",
-//         files: [
-//           {
-//             src: "ipfs://QmZZzKrnkXedm1hFktDd8fGiag3JzqqX5tJ3S3nzKC7UJz",
-//             name: "FHW85843737",
-//             mediaType: "image/svg+xml",
-//           },
-//         ],
-//       },
-//     },
-//   },
-//   {
-//     name: "#93700844",
-//     collection: "Stained Glass",
-//     saleInfo: {
-//       price: "25000000",
-//       status: "Sold",
-//     },
-//     src: "images/93700844.svg",
-//     tags: ["Generative", "Stained Glass", "Triangles"],
-//     createdAt: timestampAt("2021-8-13"),
-//     token: {
-//       policyID: "fc7f00513d26a5c4de57a5863f0849559493d5ec008951eeb65ed3f1",
-//       assetName: "FHWALGOMARBLE0489",
-//       onChainMetadata: {
-//         name: "FHW93700844",
-//         image: "ipfs://QmQoRTpB38wgMHgYDLRY5prXTrz6xFNQRQ7sTMfyHucPVS",
-//         mediaType: "image/png",
-//         files: [
-//           {
-//             src: "ipfs://QmVYSR7KVCAgYnqfDXKgwJivTXCSXvoR2GgjGHJf743tz1",
-//             name: "FHW93700844",
-//             mediaType: "image/svg+xml",
-//           },
-//         ],
-//       },
-//     },
-//   },
-// ];
