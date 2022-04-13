62e75a09b83d7831ec147da327297c0a5d6cd7a2
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2022-04-13 11:07:11 +0200

    Remove unused testmatch

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: "vite-jest",
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   testMatch: [
-    "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
     "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
     "<rootDir>/Seed/**/*.{spec,test}.{js,jsx,ts,tsx}",
   ],
